cf77513c5da627cee2901d6df1349796d85b47af
TejasAnand <anandtejas455@gmail.com>
2023-08-04 12:51:30 +0530

    walmart_search

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ load_dotenv()
 SERPAPI_API_KEY = os.getenv('SERPAPI_API_KEY')
 
 from serpapi import GoogleSearch
+
+def walmar_search_results(query: str) -> list:
+    params = {
+        'api_key': os.getenv('SERPAPI_API_KEY'), 
+        'engine': 'walmart',                      
+        'query': query,                          
+    }
+
+    search = GoogleSearch(params)       
+
+    #converting data in json from api to a dictionary           
+    results = search.get_dict()                    
+    
+    return results.get('organic_results', [])
