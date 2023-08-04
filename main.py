464bc445a04d426aa1306c55e871a68a46d58600
TejasAnand <anandtejas455@gmail.com>
2023-08-04 13:18:53 +0530

    ebay_results

--- main.py
+++ main.py
@@ -25,19 +25,17 @@ def walmart_search_results(query: str) -> list:
     
     return results.get('organic_results', [])
 
-def homedepot_results(query: str) -> list:
+def get_ebay_results(query: str) -> list:
     params = {
-        'api_key': os.getenv('SERPAPI_API_KEY'), 
-        'engine': 'home_depot',                      
-        'query': query,                          
+        'api_key': os.getenv('SERPAPI_API_KEY'),    # https://serpapi.com/manage-api-key
+        'engine': 'ebay',                           # search engine
+        '_nkw': query,                              # search query
+        'ebay_domain': 'ebay.com',                  # ebay domain
     }
 
-    search = GoogleSearch(params)       
-
-    #converting data in json from api to a dictionary           
-    results = search.get_dict()                    
+    search = GoogleSearch(params)                     # data extraction on the SerpApi backend
+    results = search.get_dict()                     # JSON -> Python dict
     
-    return results["products"]
-
+    return results.get('organic_results', [])
 
 
