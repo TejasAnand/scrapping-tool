a12240fc68773b9a79b89f069096799f23f48afd
TejasAnand <anandtejas455@gmail.com>
2023-08-04 12:43:07 +0530

    api lock/.env

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #using serapi to scrape home depot and walmart
-
+from dotenv import load_dotenv
 import time
 import Levenshtein #to check similarity of query entered by user
 import os
 import pandas as pd
+
+load_dotenv()
+
+SERPAPI_API_KEY = os.getenv('SERPAPI_API_KEY')
+
+from serpapi import GoogleSearch
